2c8566b7fc014aab45872764666d2d587e85ee94
sauliakarina <sauliakarina@gmail.com>
2018-08-31 09:54:51 +0700

    d

--- application/controllers/C_login.php
+++ application/controllers/C_login.php
@@ -42,7 +42,7 @@ class C_login extends CI_Controller{
                     $sess_data['logged_in'] = 'Sudah Masuk';
                     $sess_data['no_karyawan'] = $sess->no_karyawan;
                     $sess_data['status_user'] = $sess->status_user;
-                    $sess_data['id_layanan'] = $sess->id_layanan;
+                    //$sess_data['id_layanan'] = $sess->id_layanan;
                 }
                 $this->session->set_userdata($sess_data); 
                  /*redirect(base_url("c_main/home"));*/
